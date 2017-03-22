7049f9180eeed400c1a03eb0d685014677ff8f98
BY <beyearn@gmail.com>
2017-03-22 15:49:05 +0800

    保持屏幕常亮的两个方法

--- app/src/main/java/com/rengwuxian/rxjavasamples/MainActivity.java
+++ app/src/main/java/com/rengwuxian/rxjavasamples/MainActivity.java
@@ -1,32 +1,41 @@
 package com.rengwuxian.rxjavasamples;
 
-import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.support.design.widget.TabLayout;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
-import com.rengwuxian.rxjavasamples.module.token_advanced_5.TokenAdvancedFragment;
-import com.rengwuxian.rxjavasamples.module.token_4.TokenFragment;
 import com.rengwuxian.rxjavasamples.module.cache_6.CacheFragment;
-import com.rengwuxian.rxjavasamples.module.zip_3.ZipFragment;
 import com.rengwuxian.rxjavasamples.module.elementary_1.ElementaryFragment;
 import com.rengwuxian.rxjavasamples.module.map_2.MapFragment;
+import com.rengwuxian.rxjavasamples.module.token_4.TokenFragment;
+import com.rengwuxian.rxjavasamples.module.token_advanced_5.TokenAdvancedFragment;
+import com.rengwuxian.rxjavasamples.module.zip_3.ZipFragment;
 
-import butterknife.Bind;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @Bind(android.R.id.tabs) TabLayout tabLayout;
-    @Bind(R.id.viewPager) ViewPager viewPager;
-    @Bind(R.id.toolBar) Toolbar toolBar;
+    @BindView(android.R.id.tabs) TabLayout tabLayout;
+    @BindView(R.id.viewPager) ViewPager viewPager;
+    @BindView(R.id.toolBar) Toolbar toolBar;
+    private boolean toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         ButterKnife.bind(this);
 
         setSupportActionBar(toolBar);
@@ -78,5 +87,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         tabLayout.setupWithViewPager(viewPager);
+
+        PowerManager pm = (PowerManager) MainActivity.this.getSystemService(Context.POWER_SERVICE);  //需要<uses-permission android:name="android.permission.WAKE_LOCK"/>
+        final PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.FULL_WAKE_LOCK, "TEST");  // 这些屏显的flag自行调整
+        //是否需计算锁的数量
+        wl.setReferenceCounted(false);
+
+        toggle = false;
+
+        //对Toolbar左边的导航图标进行监听
+        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!toggle){
+                    wl.acquire();
+                }else {
+                    wl.release();
+                }
+                toggle =!toggle;
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e("onDestroy","onDestroy");
+        super.onDestroy();
     }
 }
